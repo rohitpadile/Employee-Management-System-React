0968779813c361280d4aaa62a6937c85456cefd6
Rohit Padile <rohitpadile24@gmail.com>
2024-06-25 12:43:16 +0530

    Router, Routes, Route from react-router-dom for component ListEmployeeComponent

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import  ListEmployeeComponent from './components/ListEmployeeComponent';
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
 
+
+//Switch(Routes in v6) from react-router-dom ensures that only one component renders at the same time
 function App() { //functional component
   return (
     <div>
-      <HeaderComponent/>
-      <div className="container">
-        <ListEmployeeComponent/>
-      </div>
-      <FooterComponent/>
+      <Router>
+          <HeaderComponent/>
+          <div className="container">
+            <Routes>  
+              {/* http://localhost:3000/employees */}
+                  <Route path='/' element={<ListEmployeeComponent/>}/>
+                  <Route path='/employees' element={<ListEmployeeComponent/>}/>
+            </Routes>
+          </div>
+          <FooterComponent/>
+      </Router>
     </div>
   );
 }
